b5e4e0219a98d7a89b68a5f96609ffca5df81b6e
AliRzaev <alirzaev1996@gmail.com>
2019-02-27 10:19:04 +0300

    Добавил класс ProcessesTask

--- schedulers/utils/tasks.h
+++ schedulers/utils/tasks.h
@@ -12,40 +12,45 @@
 #include "../algo/memory/exceptions.h"
 #include "../algo/memory/requests.h"
 #include "../algo/memory/strategies.h"
+#include "../algo/processes/exceptions.h"
+#include "../algo/processes/requests.h"
+#include "../algo/processes/strategies.h"
 #include "exceptions.h"
 
 namespace Utils {
-using namespace MemoryManagement;
+namespace Memory = MemoryManagement;
+namespace Processes = ProcessesManagement;
 
 class MemoryTask {
 private:
-  StrategyPtr _strategy;
+  Memory::StrategyPtr _strategy;
 
   uint32_t _completed;
 
-  MemoryState _state;
+  Memory::MemoryState _state;
 
-  std::vector<Request> _requests;
+  std::vector<Memory::Request> _requests;
 
-  MemoryTask(StrategyPtr strategy, uint32_t completed, const MemoryState &state,
-             const std::vector<Request> requests)
+  MemoryTask(Memory::StrategyPtr strategy, uint32_t completed,
+             const Memory::MemoryState &state,
+             const std::vector<Memory::Request> requests)
       : _strategy(strategy), _completed(completed), _state(state),
         _requests(requests) {}
 
 public:
-  static MemoryTask create(StrategyPtr strategy, uint32_t completed,
-                           const MemoryState &state,
-                           const std::vector<Request> requests) {
+  static MemoryTask create(Memory::StrategyPtr strategy, uint32_t completed,
+                           const Memory::MemoryState &state,
+                           const std::vector<Memory::Request> requests) {
     return {strategy, completed, state, requests};
   }
 
-  static void validate(StrategyPtr strategy, uint32_t completed,
-                       const MemoryState &state,
-                       const std::vector<Request> requests) {
+  static void validate(Memory::StrategyPtr strategy, uint32_t completed,
+                       const Memory::MemoryState &state,
+                       const std::vector<Memory::Request> requests) {
     if (requests.size() < completed) {
       throw TaskException("COMPLETED_OOR");
     }
-    auto currentState = MemoryState::initial();
+    auto currentState = Memory::MemoryState::initial();
     try {
       for (auto req = requests.begin(); req != requests.begin() + completed;
            ++req) {
@@ -54,19 +59,19 @@ public:
       if (currentState != state) {
         throw TaskException("STATE_MISMATCH");
       }
-    } catch (MemoryManagement::BaseException &ex) {
+    } catch (Memory::BaseException &ex) {
       throw TaskException(ex.what());
     }
   }
 
-  StrategyPtr strategy() const { return _strategy; }
+  Memory::StrategyPtr strategy() const { return _strategy; }
 
   // кол-во выполненных заданий
   uint32_t completed() const { return _completed; }
 
-  const MemoryState &state() const { return _state; }
+  const Memory::MemoryState &state() const { return _state; }
 
-  const std::vector<Request> &requests() const { return _requests; }
+  const std::vector<Memory::Request> &requests() const { return _requests; }
 
   nlohmann::json dump() const {
     nlohmann::json obj;
@@ -94,7 +99,7 @@ public:
 
   bool done() const { return _completed == _requests.size(); }
 
-  std::optional<MemoryTask> next(const MemoryState &state) const {
+  std::optional<MemoryTask> next(const Memory::MemoryState &state) const {
     if (done()) {
       return *this;
     }
@@ -110,14 +115,83 @@ public:
       return std::nullopt;
     }
   }
-};
+}; // namespace Process=ProceclassMemoryTask
 
 class ProcessesTask {
+private:
+  Processes::StrategyPtr _strategy;
+
+  uint32_t _completed;
+
+  Processes::ProcessesState _state;
+
+  std::vector<Processes::Request> _requests;
+
+  ProcessesTask(Processes::StrategyPtr strategy, uint32_t completed,
+                const Processes::ProcessesState &state,
+                const std::vector<Processes::Request> requests)
+      : _strategy(strategy), _completed(completed), _state(state),
+        _requests(requests) {}
+
 public:
+  static ProcessesTask create(Processes::StrategyPtr strategy,
+                              uint32_t completed,
+                              const Processes::ProcessesState &state,
+                              const std::vector<Processes::Request> requests) {
+    return {strategy, completed, state, requests};
+  }
+
   nlohmann::json dump() const {
     nlohmann::json obj;
-    obj["type"] = "PROCESS_TASK";
+
+    obj["type"] = "PROCESSES_TASK";
+
+    obj["strategy"] = strategy()->toString();
+
+    obj["completed"] = completed();
+
+    obj["state"] = state().dump();
+
+    obj["requests"] = nlohmann::json::array();
+
+    for (auto request : requests()) {
+      std::visit(
+          [&obj](const auto &request) {
+            obj["requests"].push_back(request.dump());
+          },
+          request);
+    }
+
     return obj;
+  }
+
+  Processes::StrategyPtr strategy() const { return _strategy; }
+
+  // кол-во выполненных заданий
+  uint32_t completed() const { return _completed; }
+
+  const Processes::ProcessesState &state() const { return _state; }
+
+  const std::vector<Processes::Request> &requests() const { return _requests; }
+
+  bool done() const { return _completed == _requests.size(); }
+
+  std::optional<ProcessesTask>
+  next(const Processes::ProcessesState &state) const {
+    if (done()) {
+      return *this;
+    }
+    try {
+      auto request = _requests[_completed];
+      auto expected = _strategy->processRequest(request, _state);
+      if (expected == state) {
+        return ProcessesTask{_strategy, _completed + 1, expected, _requests};
+      } else {
+        return std::nullopt;
+      }
+    } catch (...) {
+      return std::nullopt;
+    }
   }
 };
 

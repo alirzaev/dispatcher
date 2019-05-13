1e7e4eb5e73a59f701dadb1939f40f9bb9a8a9b2
AliRzaev <alirzaev1996@gmail.com>
2019-05-13 20:55:26 +0300

    Улучшил CLI генератора заданий
    Задания выдаются в виде JSON-массива

--- generator/main.cpp
+++ generator/main.cpp
@@ -9,7 +9,12 @@
 using namespace std;
 
 int main() {
-  cout << setw(4) << Generators::MemoryTask::generate(40).dump() << setw(4)
-       << Generators::ProcessesTask::generate(40).dump();
+  nlohmann::json tasks = nlohmann::json::array();
+
+  tasks.push_back(Generators::MemoryTask::generate(40).dump());
+  tasks.push_back(Generators::ProcessesTask::generate(40, false).dump());
+  tasks.push_back(Generators::ProcessesTask::generate(40, true).dump());
+
+  cout << setw(4) << tasks;
   return 0;
 }

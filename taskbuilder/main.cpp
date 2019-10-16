af54b70bbf3b77af89bc10a00286d2dbe807941d
AliRzaev <alirzaev1996@gmail.com>
2019-10-16 19:16:31 +0300

    feat(taskbuilder): настроил перевод стандартных элементов Qt

--- taskbuilder/main.cpp
+++ taskbuilder/main.cpp
@@ -2,6 +2,8 @@
 
 #include <QApplication>
 #include <QEventLoop>
+#include <QLibraryInfo>
+#include <QTranslator>
 
 #include "dialogs/tasktypechooser.h"
 #include "mainwindow.h"
@@ -9,6 +11,12 @@
 int main(int argc, char *argv[]) {
   QApplication a(argc, argv);
   a.setAttribute(Qt::AA_DisableWindowContextHelpButton);
+
+  QTranslator translator;
+  if (translator.load("qt_" + QLocale::system().name(),
+                      QLibraryInfo::location(QLibraryInfo::TranslationsPath))) {
+    a.installTranslator(&translator);
+  }
 
   MainWindow w;
   w.show();
